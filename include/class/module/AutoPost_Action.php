474ab1a707c74afcb62e5276afe7824b88a36295
Elite-AI-August <carollhwrd@gmail.com>
2015-07-03 07:53:41 +0900

    Fixed the wrong text domain.

--- include/class/module/AutoPost_Action.php
+++ include/class/module/AutoPost_Action.php
@@ -2,9 +2,9 @@
 /**
  * The class that defines the action of Delete Posts for the Task Scheduler plugin.
  * 
- * @package     Task Scheduler
- * @copyright   Copyright (c) 2014, <Michael Uno>
- * @author        Michael Uno
+ * @package      Task Scheduler
+ * @copyright    Copyright (c) 2014-2015, Michael Uno
+ * @author       Michael Uno
  * @authorurl    http://michaeluno.jp
  * @since        1.0.0
  */
@@ -27,14 +27,14 @@ class AutoPost_Action extends TaskScheduler_Action_Base {
      * This will be called when displaying the action in an pull-down select option, task listing table, or notification email message.
      */
     public function getLabel( $sLabel ) {
-        return __( 'Auto Post', 'task-scheduler' );
+        return __( 'Auto Post', 'auto-post' );
     }
 
     /**
      * Returns the description of the module.
      */
     public function getDescription( $sDescription ) {
-        return __( 'Creates posts automatically.', 'task-scheduler' );
+        return __( 'Creates posts automatically.', 'auto-post' );
     }    
     
     /**
@@ -46,30 +46,36 @@ class AutoPost_Action extends TaskScheduler_Action_Base {
     public function doAction( $sExitCode, $oRoutine ) {
         
         $_aRoutineMeta        = $oRoutine->getMeta();
-        $_aRoutineArguments   = isset( $_aRoutineMeta[ $this->sSlug ] ) ? $_aRoutineMeta[ $this->sSlug ] : array();    // the task specific options(arguments)
+        $_aRoutineArguments   = isset( $_aRoutineMeta[ $this->sSlug ] ) 
+            ? $_aRoutineMeta[ $this->sSlug ]
+            : array();    // the task specific options(arguments)
+            
         if ( 
             ! isset(    
                 $_aRoutineMeta[ $this->sSlug ],
-                $_aRoutineArguments['auto_post_content'],
-                $_aRoutineArguments['auto_post_subject'],
-                $_aRoutineArguments['auto_post_term_ids'],
-                $_aRoutineArguments['auto_post_post_type'],
-                $_aRoutineArguments['auto_post_post_status'],
-                $_aRoutineArguments['auto_post_author']
+                $_aRoutineArguments[ 'auto_post_content' ],
+                $_aRoutineArguments[ 'auto_post_subject' ],
+                $_aRoutineArguments[ 'auto_post_term_ids' ],
+                $_aRoutineArguments[ 'auto_post_post_type' ],
+                $_aRoutineArguments[ 'auto_post_post_status' ],
+                $_aRoutineArguments[ 'auto_post_author' ]
             ) 
         ) {
             return 0;    // failed
         }
 
         // the value looks like this: [{"id":1,"name":"admin"}]
-        $_aAuthor = json_decode( $_aRoutineArguments['auto_post_author'] , true );
+        $_aAuthor = json_decode( 
+            $_aRoutineArguments[ 'auto_post_author' ], 
+            true 
+        );
                 
         $_iPostID = wp_insert_post(
             array(
-                'post_title'    => $_aRoutineArguments['auto_post_subject'],
-                'post_content'  => $_aRoutineArguments['auto_post_content'],
-                'post_status'   => $_aRoutineArguments['auto_post_post_status'],
-                'post_author'   => $_aAuthor[ 0 ]['id'],
+                'post_title'    => $_aRoutineArguments[ 'auto_post_subject' ],
+                'post_content'  => $_aRoutineArguments[ 'auto_post_content' ],
+                'post_status'   => $_aRoutineArguments[ 'auto_post_post_status' ],
+                'post_author'   => $_aAuthor[ 0 ][ 'id' ],
                 
                 // Do not set any taxonomy terms. Note that still 'uncategorized' gets assigned automatically.
                 'tax_input'     => array(),
